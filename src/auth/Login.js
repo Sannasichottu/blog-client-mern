73bcabc2ded6d468939fd0aa1f10fd94caa051d9
Sannasi <sannasichottu@gmail.com>
2023-02-17 13:02:40 +0530

    user-auth

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,10 +1,46 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  async function login(e) {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8000/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    if (response.ok) {
+      setRedirect(true);
+    } else {
+      alert("wrong credentials");
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={"/"} />;
+  }
+
   return (
     <div>
-      <form className="login">
+      <form className="login" onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Login</button>
       </form>
     </div>
--- src/auth/Register.js
+++ src/auth/Register.js
@@ -1,12 +1,47 @@
+import { useState } from "react";
+
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(e) {
+    e.preventDefault();
+
+    const response = await fetch("http://localhost:8000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status === 200) {
+      alert("Registration Success");
+    } else {
+      alert("Registration failed");
+    }
+  }
   return (
     <div>
-      <form className="register">
+      <form className="register" onSubmit={register}>
         <h1>Register</h1>
-        <input type="text" placeholder="Enter your Username" />
-        <input type="email" placeholder="Enter your Email" />
+        <input
+          type="text"
+          placeholder="Enter your Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Enter your Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-        <input type="password" placeholder="Enter your password" />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Register</button>
       </form>
     </div>
